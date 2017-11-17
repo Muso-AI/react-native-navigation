f9e13c05982e297c86a75f64592d0b82a6604019
bdavid68 <b.david6008@gmail.com>
2017-11-18 02:53:47 +1000

    add showlightbox animations

--- src/deprecated/platformSpecificDeprecated.android.js
+++ src/deprecated/platformSpecificDeprecated.android.js
@@ -475,6 +475,8 @@ function showLightBox(params) {
     }
   }
   params.tapBackgroundToDismiss = _.get(params, 'style.tapBackgroundToDismiss') || false;
+  params.animationIn = _.get(params, 'style.animationIn') || '';
+  params.animationOut = _.get(params, 'style.animationOut') || '';
   newPlatformSpecific.showLightBox(params);
 }
 
